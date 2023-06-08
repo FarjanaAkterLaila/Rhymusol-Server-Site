50d1b0daef81d71596eb2cf606b73cab4130932a
FarjanaAkterLaila <farjanaakter7275@gmail.com>
2023-06-09 01:40:15 +0600

    update

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express') ;
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+require('dotenv').config()
+// middleware
+app.use(cors());
+app.use(express.json());
+
+console.log(process.env.DB_USER)
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zmwk4eu.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    const classesCollection = client.db('musicDb').collection('classes');
+    const cardCollection = client.db('musicDb').collection('addcard');
+    
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
+    
+    app.get('/classes', async (req, res) => {
+      const cursor = classesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+  })
+
+  app.post('/addcart',async(req,res)=>{
+    const iteam = req.body;
+    console.log(iteam);
+    const result = await cardCollection.insertOne(iteam)
+    res.send(result);
+  })
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/',(req,res)=>{
+    res.send("school setting")
+})
+app.listen(port,()=>{
+    console.log(`school is sitting on port: ${port}`);
+})
